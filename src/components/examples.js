fd9d270124373f445f215086b7f9f955ce75bcc1
arata <arata04962@gmail.com>
2020-03-12 16:28:08 +0900

    [add] Mandelbrot example

--- src/components/examples.js
+++ src/components/examples.js
@@ -95,4 +95,58 @@ module.exports = [
             '}\n' +
             '\n',
     },
+    {
+        name: 'Mandelbrot',
+        program:
+            'import "std"\n' +
+            '\n' +
+            'fun main() {\n' +
+            '  mandel(-2.3, -1.3, 0.05, 0.07)\n' +
+            '}\n' +
+            '\n' +
+            'fun mandel(realstart, imagstart, realmag, imagmag: float) {\n' +
+            '  mandelHelp(realstart, realstart+realmag*78.0, realmag,\n' +
+            '             imagstart, imagstart+imagmag*48.0, imagmag)\n' +
+            '}\n' +
+            '\n' +
+            'fun mandelHelp(xMin, xMax, xStep, yMin, yMax, yStep: float) {\n' +
+            '    for var y = yMin; y < yMax; y += yStep {\n' +
+            '    for var x = xMin; x < xMax; x += xStep {\n' +
+            '      var d = mandelConverge(x, y)\n' +
+            '      printDensity(d)\n' +
+            '    }\n' +
+            '    print("\\n")\n' +
+            '  }\n' +
+            '}\n' +
+            '\n' +
+            'fun mandelConverge(real, imag: float): float {\n' +
+            '  return mandelConverger(real, imag, 0.0, real, imag)\n' +
+            '}\n' +
+            '\n' +
+            'fun mandelConverger(real, imag, iters, creal, cimag: float): float {\n' +
+            '  if iters > 255.0 {\n' +
+            '    return iters;\n' +
+            '  }\n' +
+            '  if real*real + imag*imag > 4.0 {\n' +
+            '    return iters;\n' +
+            '  }\n' +
+            '  \n' +
+            '  return mandelConverger(\n' +
+            '    real*real - imag*imag + creal,\n' +
+            '    2.0*real*imag + cimag,\n' +
+            '    iters + 1.0, creal, cimag)\n' +
+            '}\n' +
+            '\n' +
+            'fun printDensity(d: float) {\n' +
+            '  if d > 8.0 {\n' +
+            '    print(" ")\n' +
+            '  } else if d > 4.0 {\n' +
+            '    print(".")\n' +
+            '  } else if d > 2.0 {\n' +
+            '    print("+")\n' +
+            '  } else {\n' +
+            '    print("*")\n' +
+            '  }\n' +
+            '}\n'
+    }
 ]
