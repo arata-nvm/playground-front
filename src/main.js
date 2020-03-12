5f859349fa846730d58b991e6b83fbbcee33d2e7
arata <arata04962@gmail.com>
2020-03-12 20:00:48 +0900

    [add] Google Analitics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAnalytics from "vue-analytics";
 
 import "prismjs";
 import "prismjs/themes/prism.css";
@@ -7,6 +8,10 @@ import "prismjs/themes/prism.css";
 import "vue-prism-editor/dist/VuePrismEditor.css";
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-158885575-3',
+})
 
 new Vue({
   render: h => h(App),
